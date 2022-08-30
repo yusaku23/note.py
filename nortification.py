0e8b274c25dc11fe4a15a5679bd2285bd2651c42
yusaku23 <100559616+yusaku23@users.noreply.github.com>
2022-08-30 21:11:09 +0900

    create new file

---
+++ nortification.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import os
+
+blog_link = [https://note.com/kira_se/n/nd9706317963d,https://note.com/horiday018/all,https://note.com/tsukishimacoffee/all,
+https://note.com/mypro1/all,https://note.com/manunitact/all,https://note.com/akisuke0925/all,https://note.com/hitohira_aka_2/all,
+https://note.com/kotyatv/all,https://note.linuc.org/all]
