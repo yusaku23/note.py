d99ce1f54184dc2e4eb7722688dd9054be9bbc5e
yusaku23 <100559616+yusaku23@users.noreply.github.com>
2022-09-02 14:46:28 +0900

    edit blog_link visual

--- nortification.py
+++ nortification.py
@@ -5,9 +5,16 @@ import os
 
 date = datetime.datetime.now()
 
-blog_link = ["https://note.com/kira_se/n/nd9706317963d","https://note.com/horiday018/all","https://note.com/tsukishimacoffee/all",
-"https://note.com/mypro1/all","https://note.com/manunitact/all","https://note.com/akisuke0925/all","https://note.com/hitohira_aka_2/all",
-"https://note.com/kotyatv/all"]
+blog_link = [
+  "https://note.com/kira_se/all"
+  , "https://note.com/horiday018/all"
+  , "https://note.com/tsukishimacoffee/all"
+  , "https://note.com/mypro1/all"
+  , "https://note.com/manunitact/all"
+  , "https://note.com/akisuke0925/all"
+  , "https://note.com/hitohira_aka_2/all"
+  , "https://note.com/kotyatv/all"
+]
 
 for link in blog_link:
   response = requests.get(link)
